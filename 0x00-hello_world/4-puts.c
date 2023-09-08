a47fdf7379102cc279c662d54ba3794f6c1bd67d
Edmund15 <annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.com>
2023-09-08 01:59:10 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - main entry point
+*main - the output
+*return : always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
