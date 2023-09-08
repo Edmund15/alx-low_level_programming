1c32a06bf60438cf980cb87ec4c67b518e937dae
Edmund15 <annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.com>
2023-09-08 02:49:55 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ * Description: This is the main function that prints a message
+ *              to the standard output.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
