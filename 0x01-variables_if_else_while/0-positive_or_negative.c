d650ab5545789d1469238d337aef8331c4f3b077
Edmund15 <annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.com>
2023-09-09 03:41:38 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,26 +13,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* Your code goes here */
+
 
 	if (n > 0)
-
 	{
 	    printf("%d is positive\n", n);
 	}
-
 	else if (n == 0)
-
 	{
 	    printf("%d is zero\n", n);
 	}
-
 	else
-
 	{
 	    printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
 
