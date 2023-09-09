f0cfe138e69604ee993571029787cae150c01253
Edmund15 <annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.comgit config --global user.email annohboateng15@gmail.com>
2023-09-09 07:24:42 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: Prints all possible different combinations of two digits.
- * Return: Always 0
- */
+  * main - Entry point
+  * Description: Prints all possible different combinations of two digits.
+  * Return: Always 0
+  */
 int main(void)
 {
 	int num1, num2;
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
 			if (num1 == 8 || num2 == 9)
 			{
-				continue;
 				putchar(',');
 				putchar(' ');
 			}
