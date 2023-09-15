8e5468af22c313da6e6efeef1049b09fdeb1b3cb
Edmund15 <annohboateng15@gmail.com>
2023-09-15 04:37:54 +0000

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,22 +2,33 @@
 #include <math.h>
 
 /**
-  *main - The prime factors of 1231952
-  *Return: always 0
-  */
+ * main - Find and print the largest prime factor of 1231952
+ * Return: Always 0
+ */
 int main(void)
 {
 	long x, maxf;
 	long number = 1231952;
 	double square = sqrt(number);
 
-	for (x = 2; x <= square; x++)
+	maxf = 2;
+
+	while (number % 2 == 0)
 	{
-		if (number % x == 0)
+		number /= 2;
+	}
+
+	for (x = 3; x <= square; x += 2)
+	{
+		while (number % x == 0)
 		{
 			maxf = x;
 			number /= x;
 		}
+	}
+	if (number > 1)
+	{
+		maxf = number;
 	}
 	printf("%ld\n", maxf);
 	return (0);
