8191815b4b693cc14476a94c8d0645ba7fce7bfb
Edmund15 <annohboateng15@gmail.com>
2023-09-15 04:17:56 +0000

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+  *main - The prime factors of 1231952
+  *Return: always 0
+  */
+int main(void)
+{
+	long x, maxf;
+	long number = 1231952;
+	double square = sqrt(number);
+
+	for (x = 2; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
