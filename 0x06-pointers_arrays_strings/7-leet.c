3c78af02053278e863d8dd9e7215a4d64f9dbb08
Edmund15 <annohboateng15@gmail.com>
2023-09-23 00:35:49 +0000

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,25 +3,26 @@
 /**
   *leet - main function
   *
-  * @n: Function parameter
+  * @str: Function parameter
   *
-  *Return: value of n
+  *Return: value of str
  */
-char *leet(char *n)
+char *leet(char *str)
 {
 	int k, r;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
-	
-	for (k = 0; n[k] != '\0'; k++)
+
+	for (k = 0; str[k] != '\0'; k++)
 	{
 		for (r = 0; r < 10; r++)
 		{
-			if (n[k] == s1[r])
+			if (str[k] == s1[r])
 			{
-				n[k] = s2[r];
+				str[k] = s2[r];
+				break;
 			}
 		}
 	}
-	return (n);
+	return (str);
 }
