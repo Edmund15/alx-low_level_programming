9e7f608bcc9725e764183bce88629abd83ec1ed1
Edmund15 <annohboateng15@gmail.com>
2023-10-10 03:06:29 +0000

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
-  *_strien - returns the length of a string
+  *_strlen - returns the length of a string
   *@s: string to evaluate
   *Return: the length of string
   */
-int_strien(char *s)
+int _strlen(char *s)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
-		i++
+		i++;
 	}
 	return (i);
 }
@@ -30,17 +29,12 @@ int_strien(char *s)
   */
 char *_strcpy(char *dest, const char *src)
 {
-	int len = 0,
-	int i;
+	int i = 0;
 
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i <= len; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
@@ -59,8 +53,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	len1 = _strien(name);
-	len2 = _strien(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,20 @@
-#include <stdlib.h>
-#include "dog.h"
-/**
-  *init_dog - initializes a varible of type struct dog
-  *@d: pointer to struct dog to initialize
-  *@name: name to initialize
-  *@age: age to initiaze
-  *@owner: owner to initialize
-  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, const char *src);
+int _strlen(char *s);
+#endif
+
