1c4b348637e175d3f61d924717ed0cd57916ea3d
Edmund15 <annohboateng15@gmail.com>
2023-12-13 11:17:49 +0000

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC 0-isupper.c 1-isdigit.c 2-strchr.c 3-islower.c 4-isalpha.c 5-strstr.c 6-abs.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
