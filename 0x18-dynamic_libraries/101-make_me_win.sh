b9223fde31c53c946187474317851a16326a1810
Edmund15 <annohboateng15@gmail.com>
2023-12-13 13:16:36 +0000

    advance task2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo -e '#include <stdlib.h>\n#include <stdio.h>\n\nint rand(void) { return 6; }\n' > /tmp/rand_override.c
-gcc -shared -o /tmp/rand_override.so /tmp/rand_override.c
-
-
-LD_PRELOAD=/tmp/rand_override.so ./gm 9 8 10 24 75 9
-
+wget -P /tmp https://github.com/Edmund15/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD=/tmp/liball.so.1
